1960c4bcb808a219ecc51591feb220853bf014f8
Kim Fransman <kim.fransman@gmail.com>
2011-09-21 16:28:41 +0200

    public is now as middleware

--- lib/newsly/engine.rb
+++ lib/newsly/engine.rb
@@ -1,5 +1,10 @@
 module Newsly
   class Engine < Rails::Engine
     isolate_namespace Newsly
+
+    initializer "static assets" do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+    
   end
 end
