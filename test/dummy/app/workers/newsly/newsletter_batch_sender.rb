ae272c5435c0a3e665055419c8fb32f8bd56017e
Kim Fransman <kim.fransman@gmail.com>
2011-10-04 14:09:59 +0200

    some work on the example workers that do the work... plus small css

---
+++ test/dummy/app/workers/newsly/newsletter_batch_sender.rb
@@ -0,0 +1,15 @@
+module Newsly
+	class NewsletterBatchSender
+	  @queue = :newsletter
+	  def self.perform(newsletter_id, recipient_group, batch_size)
+
+	  	newsletter = Newsly::Newsletter.find(newsletter_id)
+	  	customer = Customer.find_or_create_by_first_name("Newsletter #{newsletter_id}")
+
+	    Newsly.get_newsletter_receipient_group(recipient_group.to_sym).call.limit(batch_size).each do |resource|
+	      Newsly::Mailer.async_deliver.build_newsletter(newsletter_id, resource.send(Newsly.resource_email_column), resource.to_liquid, {:email_type => customer.id.to_s})
+	    end
+    end
+
+	end
+end
