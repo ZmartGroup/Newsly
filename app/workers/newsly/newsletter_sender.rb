56926a5642b4c84ade5793191cbd5c5584661112
Kim Fransman <kim.fransman@gmail.com>
2011-10-05 15:57:17 +0200

    pushing

--- app/workers/newsly/newsletter_sender.rb
+++ app/workers/newsly/newsletter_sender.rb
@@ -3,7 +3,7 @@ module Newsly
 	  @queue = :newsletter
 	  def self.perform(newsletter_id, recipient_groups)
       for group in recipient_groups
-	      Newsly.get_newsletter_receipient_group(group.to_sym).call.find_each(:batch_size => 1000) do |resource|
+	      Newsly.get_newsletter_receipient_group(group.to_sym).call(newsletter_id).find_each(:batch_size => 1000) do |resource|
 	        Newsly::Mailer.async_deliver.build_newsletter(newsletter_id, resource.send(Newsly.resource_email_column), resource.to_liquid)
 	      end
     	end
--- test/dummy/app/workers/newsly/newsletter_batch_sender.rb
+++ test/dummy/app/workers/newsly/newsletter_batch_sender.rb
@@ -6,7 +6,7 @@ module Newsly
 	  	newsletter = Newsly::Newsletter.find(newsletter_id)
 	  	customer = Customer.find_or_create_by_first_name("Newsletter #{newsletter_id}")
 
-	    Newsly.get_newsletter_receipient_group(recipient_group.to_sym).call.limit(batch_size).each do |resource|
+	    Newsly.get_newsletter_receipient_group(recipient_group.to_sym).call(newsletter_id).limit(batch_size).each do |resource|
 	      Newsly::Mailer.async_deliver.build_newsletter(newsletter_id, resource.send(Newsly.resource_email_column), resource.to_liquid, {:email_type => customer.id.to_s})
 	    end
     end
