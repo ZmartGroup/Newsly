75e6794d1fc3f6bb6d7403c215c28ed2c948270c
Kim Fransman <kim.fransman@gmail.com>
2011-10-25 10:57:05 +0200

    Removed old and unused migration.

--- db/migrate/20111006122345_create_newsly_recipients.rb
+++
@@ -1,9 +0,0 @@
-class CreateNewslyRecipients < ActiveRecord::Migration
-  def change
-    create_table :newsly_recipients do |t|
-    	t.newsletter_id :integer
-    	t.email					:string
-      t.timestamps
-    end
-  end
-end
